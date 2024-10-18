1b093caf34a0ae14110570d2c8b38b707216cba5
Karan <email@karanshergill.com>
2024-10-18 16:24:44 +0530

    add tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'public-sans': "'Public Sans Variable', 'sans-serif'"
+      },
+      colors: {
+        primary: {
+          'dark-blue': 'hsl(233, 26%, 24%)',
+          'lime-green': 'hsl(136, 65%, 51%)',
+          'bright-cyan': 'hsl(192, 70%, 51%)'
+        },
+        neutral: {
+          'grayish-blue': 'hsl(233, 8%, 62%)',
+          'light-grayish,blue': 'hsl(220, 16%, 96%)',
+          'very-light-gray': 'hsl(0, 0%, 98%)'
+        }
+      },
+    },
   },
   plugins: [],
 }
