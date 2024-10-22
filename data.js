35c791bb59108c3ed44555c295a25b5950a38882
Karan <email@karanshergill.com>
2024-10-22 08:37:02 +0530

    add data

---
+++ data.js
@@ -0,0 +1,22 @@
+const navItems = [
+    {
+        "label": "home",
+        "link": "home"
+    },
+    {
+        "label": "about",
+        "link": "about"
+    },
+    {
+        "label": "contact",
+        "link": "contact"
+    },
+    {
+        "label": "blog",
+        "link": "blog"
+    },
+    {
+        "label": "careers",
+        "link": "careers"
+    }
+]
